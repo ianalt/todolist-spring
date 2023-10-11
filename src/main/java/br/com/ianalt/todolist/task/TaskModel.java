1d34117d9b8f738fadb8d44afc84cc227d6c3ec8
Ian Alt <ian.alt@orange.com>
2023-10-11 17:21:37 -0300

    created task repository, route and applied lombok

--- src/main/java/br/com/ianalt/todolist/task/TaskModel.java
+++ src/main/java/br/com/ianalt/todolist/task/TaskModel.java
@@ -3,20 +3,31 @@ package br.com.ianalt.todolist.task;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_tasks")
 public class TaskModel {
   @Id
   @GeneratedValue(generator = "UUID")
   private UUID id;
 
-  
-
-  private String title;
-  private String description;
+  @CreationTimestamp
   private LocalDateTime createdAt;
+
+  @Column(length = 50)
+  private String title;
+  
+  private String description;
+  private String priority;
+  private LocalDateTime startedAt;
   private LocalDateTime finishedAt;
+
+  private UUID userId;
 }
